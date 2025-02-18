8a3e6c5a478a34dc0cff597dbf7da9009c723d64
sjoerd <sjoerd@keystudio.nl>
2025-02-18 19:44:56 +0100

    wip in selecting a slice which we want to generate.

--- main.go
+++ main.go
@@ -36,10 +36,43 @@ func main() {
 	for i, slice := range config.Slices {
 		slices[i] = slice.Title
 	}
-
 	slicesToGenerate := Checkboxes(
 		"Wich slice would you like to generate", slices,
 	)
-	fmt.Println(slicesToGenerate)
+	//fmt.Println(slicesToGenerate)
+
+	for _, sliceName := range slicesToGenerate {
+
+		fmt.Println(sliceName)
+		for _, slice := range config.Slices {
+			if slice.Title == sliceName {
+
+				fmt.Println("=== commands ===")
+				for _, command := range slice.Commands {
+					fmt.Println(command.Title)
+				}
+
+				fmt.Println("=== events ===")
+				for _, event := range slice.Events {
+					fmt.Println(event.Title)
+				}
+				fmt.Println("=== readmodel ===")
+				for _, readmodel := range slice.Readmodels {
+					fmt.Println(readmodel.Title)
+				}
+				fmt.Println("=== aggregates ===")
+
+				for _, aggregate := range slice.Aggregates {
+					fmt.Println(aggregate)
+				}
+
+				//fmt.Printf("commands : %s")
+				//slice.Commands
+				//fmt.Println(slice)
+
+			}
+		}
+
+	}
 
 }
